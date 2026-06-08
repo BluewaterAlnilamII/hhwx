d3fb05c981e84548ae093fd5c64015c21f559ec5
BluewaterAlnilamII <bluewater.alnilam.ii@gmail.com>
2026-06-09 01:42:37 +0800

    Add medley Node memory profiling

--- src/lib/bandori/team-builder/medley/types.ts
+++ src/lib/bandori/team-builder/medley/types.ts
@@ -221,6 +221,12 @@ export type BandoriMedleyTeamSearchProfilingStats = {
   completedAreaItemConfigurationCount: number;
   rootUpperPrunedConfigurationCount: number;
   rootUpperBestConfigurationUpperBound: number | null;
+  lastNodeHeapUsedMiB: number | null;
+  peakNodeHeapUsedMiB: number | null;
+  lastNodeRssMiB: number | null;
+  peakNodeRssMiB: number | null;
+  lastMemoryGuardUsedMiB: number | null;
+  peakMemoryGuardUsedMiB: number | null;
   relativeGap: number | null;
   gapClosureFromBaseline: number | null;
   timeToBestScoreMs: number | null;
