0cb61d796a6bd92791cb3c719232efbf733efc45
BluewaterAlnilamII <bluewater.alnilam.ii@gmail.com>
2026-06-09 03:42:48 +0800

    Revert "Add near-frontier medley proof sweep option"
    This reverts commit e9e6318413b445df2baf483f9fcc66b2803db0ba.

--- src/lib/bandori/team-builder/medley/types.ts
+++ src/lib/bandori/team-builder/medley/types.ts
@@ -80,9 +80,6 @@ export type BandoriMedleySearchOptimizationOptions = {
   lowMemoryHighPairScanMinRecordCount?: number;
   enableLowMemoryHighPairPrefixUpper?: boolean;
   lowMemoryHighPairPrefixRecordLimit?: number;
-  enableNearFrontierProofSweep?: boolean;
-  nearFrontierProofSweepMaxGap?: number;
-  nearFrontierProofSweepRootPrefixCount?: number;
   debugExactCandidateJoinMemoryAttribution?: boolean;
   enableTrailingSameCoarseDfsOnly?: boolean;
   disableDominatedRootSkip?: boolean;
